cf5c12174f27912b2dfa6f0db4e9c04d61dfe31c
MashaReshetar <mashareshetar@gmail.com>
2020-06-17 23:25:53 +0300

    Todo

---
+++ react/my/src/Modal/Modal.js
@@ -0,0 +1,20 @@
+// import React from 'react'
+// import './Modal.css'
+
+// export default class Modal extends React.Component {
+//     render() {
+//         return (
+//             <React.Component>
+//             <button>Open Modal</button>
+
+//             <div className='modal'>
+//              <div className='modal-body'>
+//                 <h1>Modal title</h1>
+//                 <p>I am awesome modal!</p>
+//                 <button>Close modal</button>
+//             </div>
+//             </div>
+//             </React.Component>
+//         )
+//     }
+// }
